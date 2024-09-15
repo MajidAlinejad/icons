417d97d1449d6e3d60795552ae90f5a65bed1e4b
majid <alinejad.mj@gmail.com>
2024-09-15 10:14:24 +0330

    chore: delete s.tsx

--- src/s.tsx
+++
@@ -1,7 +0,0 @@
-const SvgComponent = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width={48} height={1} {...props}>
-    <title>{"Rectangle 5"}</title>
-    <path fill="#063855" fillRule="evenodd" d="M0 0h48v1H0z" />
-  </svg>
-);
-export default SvgComponent;
